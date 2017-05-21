24b2419f4c8dcd2ce3e23af4fda5d35da7d980d9
Aleksandra <aleksssandra.kurowska@gmail.com>
2017-05-21 19:04:49 +0200

    countdown timer

--- main.js
+++ main.js
@@ -4,6 +4,50 @@ var questions = document.getElementsByClassName('questions')[0];
 var areYouReady = document.getElementsByClassName('start-quiz__header')[0];
 var textBubble = document.getElementsByClassName('main-header__text-bubble')[0];
 var finishButton = document.getElementsByClassName('finish-quiz__button')[0];
+var countdownTime = document.getElementsByClassName('start-quiz__countdownTime')[0];
+var threeMinutes;
+var distance;
+
+fetch('https://cdn.rawgit.com/kdzwinel/cd08d08002995675f10d065985257416/raw/811ad96a0567648ff858b4f14d0096ba241f28ef/quiz-data.json')
+  .then(data => data.json())
+  .then(function(data){
+
+function finishQuiz() {
+  var goodAnswers = 0;
+  var badAnswers = 0;
+  for (let i = 0; i < data.questions.length; i++) {
+
+    if (typeof(data.questions[i].answers[isChosen[i]]) !== 'undefined'  &&  data.questions[i].answers[isChosen[i]].correct) {
+      goodAnswers++;
+    } else {
+      badAnswers++;
+      if (typeof(data.questions[i].answers[isChosen[i]]) !== 'undefined'){
+
+        var wrongAnswer = document.querySelector('.questions__question:nth-of-type(' + (i+1) + ')' + ' .questions__button-answer:nth-of-type(' + (isChosen[i] +1) + ')' + ' .questions__button') ;
+        console.log(wrongAnswer);
+        console.log(i);
+        console.log(isChosen[i]);
+        wrongAnswer.className += ' questions__button--wrong';
+      }
+    }
+  }
+  finishButton.classList.toggle('finish-quiz__button--disappear');
+  var finishQuiz = document.getElementsByClassName('finish-quiz')[0];
+  var result = document.createElement('div');
+  finishQuiz.appendChild(result);
+  result.className += 'result'
+  var goodResult =  document.createElement('div');
+  var badResult =  document.createElement('div');
+  result.appendChild(goodResult);
+  result.appendChild(badResult);
+  goodResult.className += 'result__good';
+  badResult.className += 'result__bad';
+  goodResult.innerHTML += 'Prawidłowe odpowiedzi:' + ' ' + goodAnswers;
+  badResult.innerHTML += 'Nieprawidłowe odpowiedzi:' + ' ' + badAnswers;
+
+  clearInterval(threeMinutes);
+  document.getElementsByClassName('start-quiz__countdownTime--active')[0].innerHTML = 'Test został zakończony';
+}
 
 
 startButton.addEventListener('click', function() {
@@ -13,12 +57,27 @@ startButton.addEventListener('click', function() {
   areYouReady.classList.toggle('start-quiz__header--active');
   textBubble.classList.toggle('main-header__text-bubble--active');
   finishButton.classList.toggle('finish-quiz__button--active');
+  countdownTime.classList.toggle('start-quiz__countdownTime--active');
+
+  var updateTime = new Date(new Date().getTime() + 180000).getTime();
+  threeMinutes = setInterval(function(){
+    var now = new Date().getTime();
+    distance = updateTime - now;
+
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementsByClassName('start-quiz__countdownTime--active')[0].innerHTML = minutes + 'm ' + seconds + 's ';
+
+    if (distance < 0) {
+      clearInterval(threeMinutes);
+      document.getElementsByClassName('start-quiz__countdownTime--active')[0].innerHTML = 'Odliczanie czasu zakończone'
+      finishQuiz();
+
+    }
+  }, 1000);
 });
 
-fetch('https://cdn.rawgit.com/kdzwinel/cd08d08002995675f10d065985257416/raw/811ad96a0567648ff858b4f14d0096ba241f28ef/quiz-data.json')
-  .then(data => data.json())
-  .then(function(data){
-    // console.log(data)
     var letters = "ABCD";
     var isChosen = [];
 
@@ -42,13 +101,16 @@ fetch('https://cdn.rawgit.com/kdzwinel/cd08d08002995675f10d065985257416/raw/811a
         buttonAnswer.appendChild(button);
         button.className += 'sg-button-secondary sg-button-secondary--small sg-button-secondary--dark-inverse questions__button';
         button.innerHTML += letters[j];
+
         button.addEventListener('click', function(event){
-          if (isChosen[i]) {
+          if (typeof(isChosen[i]) === 'number' || distance < 0){
+            // if (typeof(isChosen[i] === 'number') {
             return;
           } else {
           event.target.classList.toggle('questions__button--check');
           isChosen[i] = j;
         }
+
         });
 
         var answer = document.createElement('div');
@@ -57,39 +119,10 @@ fetch('https://cdn.rawgit.com/kdzwinel/cd08d08002995675f10d065985257416/raw/811a
         answer.innerHTML += data.questions[i].answers[j].answer;
       }
     }
+
+
+
     finishButton.addEventListener('click', function(event){
-      var goodAnswers = 0;
-      var badAnswers = 0;
-      for (let i = 0; i < data.questions.length; i++) {
-
-        if (typeof(data.questions[i].answers[isChosen[i]]) !== 'undefined'  &&  data.questions[i].answers[isChosen[i]].correct) {
-          goodAnswers++;
-        } else {
-          badAnswers++;
-          if (typeof(data.questions[i].answers[isChosen[i]]) !== 'undefined'){
-
-            var wrongAnswer = document.querySelector('.questions__question:nth-of-type(' + (i+1) + ')' + ' .questions__button-answer:nth-of-type(' + (isChosen[i] +1) + ')' + ' .questions__button') ;
-            console.log(wrongAnswer);
-            console.log(i);
-            console.log(isChosen[i]);
-            wrongAnswer.className += ' questions__button--wrong';
-          }
-        }
-      }
-      finishButton.classList.toggle('finish-quiz__button--disappear');
-      var finishQuiz = document.getElementsByClassName('finish-quiz')[0];
-      var result = document.createElement('div');
-      finishQuiz.appendChild(result);
-      result.className += 'result'
-      var goodResult =  document.createElement('div');
-      var badResult =  document.createElement('div');
-      result.appendChild(goodResult);
-      result.appendChild(badResult);
-      goodResult.className += 'result__good';
-      badResult.className += 'result__bad';
-      goodResult.innerHTML += 'Prawidłowe odpowiedzi:' + ' ' + goodAnswers;
-      badResult.innerHTML += 'Nieprawidłowe odpowiedzi:' + ' ' + badAnswers;
-
+      finishQuiz();
     })
-
   })
